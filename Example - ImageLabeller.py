aa069b95bceb0c46ea30b04ac6564fe743ef876f
Mayur Garg <mayur7garg@gmail.com>
2024-06-07 20:35:13 +0530

    Added simple example for ImageLabeller

---
+++ Example - ImageLabeller.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from MrAnnotate import ImageLabeller
+
+image_paths = list(Path("data", "image_label_ex").glob("*.jpg"))
+labels = ["Abstract", "Harry Potter", "Spider man", "Space"]
+
+il = ImageLabeller(
+    image_paths,
+    labels
+)
+
+il.run()
+
+for i in range(len(image_paths)):
+    print(f"{image_paths[i].stem} - {labels[il.img_labels[i]]}")
